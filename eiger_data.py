cd4ae9a2e74e80071f8b5489943a1a331b670159
Daron Chabot <dchabot@bnl.gov>
2014-03-28 14:10:32 -0500

    Add class to work with Eiger hdf5 data.

--- eiger_data.py
+++ eiger_data.py
@@ -11,4 +11,25 @@ def get_datasets(master_filename):
     datasets = [ d[1] for d in datatuples ]
     return datasets
 
+class Eiger_Dataset(object):
+    def __init__(self, master_filename):
+        self.master_file = master_filename
 
+        file = h5py.File(master_filename, 'r')
+        nodes = file['/entry']
+        datatuples = [item for item in nodes.items() if item[0].startswith('data_') ]
+        # sort the datatuples by dataset order - modifies object inline
+        datatuples.sort()
+        # the np.arrays are the 2nd item of the datatuple
+        self.datasets = [ d[1] for d in datatuples ]
+        self.num_datasets = len(self.datasets)
+        self.num_images = 0
+        for d in self.datasets:
+            self.num_images += d.shape[0]
+
+    # or, just access the object attribute - obj.datasets
+    def getDatasets(self):
+        return self.datasets
+
+    def __getitem__(self, item):
+        print item
